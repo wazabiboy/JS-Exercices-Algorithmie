48139fed7a9528746506c2e4d71da89b3f1b587f
wazabiboy <wazabi.boy@gmail.com>
2021-01-04 12:01:55 +0100

    alcul

---
+++ exercices_entrainement/ex.js
@@ -0,0 +1,115 @@
+//ex1 - Quelles seront les valeurs des variables A et B après chaque ligne du code suivant ? 
+
+
+let a = 3
+undefined
+let b = a+3
+undefined
+b
+6
+let a = 4
+undefined
+
+//ex2  Qu’affiche l’algorithme suivant ? 
+
+let a = 2.3
+undefined
+let b = 3.5
+undefined
+let c = a+b
+undefined
+c = 5.8
+
+//ex3 - Quelles seront les valeurs des variables A, B et C après chaque ligne du code suivant ?
+
+a = 3
+3
+b = 5
+5
+c = a + b
+8
+c = b - a
+2
+
+//ex4 Quelles seront les valeurs des variables A, B et C après chaque ligne du code suivant ?
+
+let a,b,c
+undefined
+a = 5
+5
+b = 10
+10
+c = a+b
+15
+b = a+b
+15
+a = c
+15
+
+//ex5  Quelles seront les valeurs des variables A, B et C après chaque ligne du code suivant ?
+
+let a,b,c
+undefined
+a = 5
+5
+b = 10
+10
+c = a+b
+15
+b = a+b
+15
+a = c
+15
+
+
+//ex6 uelles sont les valeurs de A et B à la fin du code suivant ? Adaptez l’algorithme pour échanger les valeurs de A et B.
+
+a = 3
+3
+b = 5
+5
+a = b
+5
+b = a 
+5
+
+//ex7 Qu’affiche l’algorithme suivant ?
+
+let a,b
+undefined
+a = "123"
+"123"
+b = "456"
+"456"
+a + b 
+"123456"
+
+//ex8 Qu’affiche l’algorithme suivant ?
+
+a = 2.3
+2.3
+b = 3.5
+3.5
+c = a + b
+5.8
+c
+
+
+// ex9  Écrire un algorithme qui détermine la catégorie sportive d’un utilisateur en fonction de son âge : 
+
+let junior = "18 à 19 ans"
+undefined
+let espoir = "20 à 22 ans"
+undefined
+let sénior = "23 à 39 ans"
+undefined
+let vétéran = "40 ans et plus"
+
+// ex10 
+// Ecrire un algorithme qui affiche les nombres de 1 à 10 par ordre croissant.
+
+for ( let a = 1; a <= 10; a++ ) {
+    console.log( a );}
+    
+// Ecrire un algorithme qui affiche les nombres de 1 à 10 par ordre décroissant.
+
